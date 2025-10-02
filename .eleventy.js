957330a0818e5929817d8c94c3c437eb23942127
muiztasarim-web <muiztasarim@gmail.com>
2025-10-02 16:25:00 +0300

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,6 @@
 module.exports = function(eleventyConfig) {
-  // Statik dosyaları kopyala
   eleventyConfig.addPassthroughCopy({"src/assets": "assets"});
   eleventyConfig.addPassthroughCopy("admin");
-
-  // Çıktıyı _site klasörüne üret
   return {
     dir: { input: "src", output: "_site", includes: "_includes" },
     htmlTemplateEngine: "njk",
